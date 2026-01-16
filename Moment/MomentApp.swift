622ad0485aecb5a00f04893d9a79f52387690244
Krijn Haasnoot <krijnhaasnoot@gmail.com>
2026-01-16 10:57:21 +0100

    Initial Commit

---
+++ Moment/MomentApp.swift
@@ -0,0 +1,17 @@
+//
+//  MomentApp.swift
+//  Moment
+//
+//  Created by Krijn Haasnoot on 16/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct MomentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MomentTests/MomentTests.swift
@@ -0,0 +1,17 @@
+//
+//  MomentTests.swift
+//  MomentTests
+//
+//  Created by Krijn Haasnoot on 16/01/2026.
+//
+
+import Testing
+@testable import Moment
+
+struct MomentTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
